feda9ff0803f5661225b3f79c665de708740bd92
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-10-28 21:00:40 +0000

    h

--- stepper_control.py
+++ stepper_control.py
@@ -21,7 +21,7 @@ print('<b>Lab 5 Stepper</b>')
 print('<br><br>')
 
 slide = {"slider1":s1, "action":act} #creates slide dictionary for slider and Led variables
-with open('step.txt', 'w') as f: 
+with open('steptest.txt', 'w') as f: 
   json.dump(slide,f) #uses json to dump dictionary into file
 
 print('<html>')
