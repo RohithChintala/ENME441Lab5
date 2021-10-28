fc2c1c894d67b9a506257f1ae58b2c702c0648e4
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-10-28 22:34:40 +0000

    s

--- stepper_control_back.py
+++ stepper_control_back.py
@@ -12,7 +12,7 @@ while True: #runs continuously
     data = json.load(f) #sets data to be loaded from json dump file
     angle = int(data['slider1'])
     act = data['action']
-  S = Stepper(angle)
+  S = Stepper()
   S.goAngle(angle)
   #sleep(2)
   #if data['action'] == 'r':
