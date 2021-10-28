a4fa584164f1964c45b018ca80dee62af3e14022
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-10-28 19:52:02 +0000

    s

--- ddddddddstepper.py
+++ ddddddddstepper.py
@@ -36,8 +36,7 @@ def halfstep(dir):
   delay_us(1000)
 
 def moveSteps(steps, dir):
-  #move the actuation sequence a given number of half steps 
-  for step in steps:
+  for step in range(steps):
     halfstep(dir)
 
 
