ab222457d4b823f86a4d0ce93c59a138b5993a46
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-10-29 17:22:07 +0000

    s

--- ddddddddstepper.py
+++ ddddddddstepper.py
@@ -108,8 +108,8 @@ class Stepper:
         moveSteps(step,-1)
         Stepper.currentangle = angle
   def zero(self):
-    #GPIO.output(27, 1)
-    #sleep(.5)
+    GPIO.output(27, 1)
+    sleep(.5)
     while self.adc.read(0) < 170: #check to see what normal value
       GPIO.output(27, 1)
       moveSteps(20,1)
